730aaff4a54c097ad5392443c4ce330a992b432f
Hammad Rafique <codingmagician0@gmail.com>
2025-05-15 14:05:28 +0500

    update backend

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,17 @@
-const exampleRoute = require('./example');
+const modelRoute = require('./model');
+const { ipcMain, dialog } = require('electron');
 
 module.exports = function setupRoutes(ipcMain) {
-  ipcMain.handle('example:getMessage', exampleRoute.getMessage);
-  ipcMain.handle('example:generateScenes', exampleRoute.generateScenes);
-  ipcMain.handle('example:refreshScene', exampleRoute.refreshScene);
-  ipcMain.handle('example:updateScene', exampleRoute.updateScene);
-  ipcMain.handle('example:getStory', exampleRoute.getStory);
+  ipcMain.handle('model:getMessage', modelRoute.getMessage);
+  ipcMain.handle('model:generateScenes', modelRoute.generateScenes);
+  ipcMain.handle('model:refreshScene', modelRoute.refreshScene);
+  ipcMain.handle('model:updateScene', modelRoute.updateScene);
+  ipcMain.handle('model:getStory', modelRoute.getStory);
+  ipcMain.handle('model:transcribeScene', modelRoute.transcribeScene);
+  ipcMain.handle('dialog:pickFile', async () => {
+    return await dialog.showOpenDialog({
+      properties: ['openFile'],
+      filters: [{ name: 'WAV Files', extensions: ['wav', 'mp3'] }]
+    });
+  });
 };
