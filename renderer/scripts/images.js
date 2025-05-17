c928443a16d263b2843f0da950c1e0d6a8f4c2f4
Hammad Rafique <codingmagician0@gmail.com>
2025-05-17 13:52:16 +0500

    update image rendering

--- renderer/scripts/images.js
+++ renderer/scripts/images.js
@@ -1,6 +1,11 @@
-import { generateImage } from './api.js';
+import { generateImage, regenerateImage } from './api.js';
 
 export async function generate_image(story_id, scene_id) {
     const res = await generateImage(story_id, scene_id);
+    return res;
+}
+
+export async function regenerate_image(story_id, scene_id) {
+    const res = await regenerateImage(story_id, scene_id);
     return res;
 }
